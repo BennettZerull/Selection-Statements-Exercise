0514d9ee65c0ea1d06f7cb12725fd55ded4ef041
Bennett Zerull <zerullbe@gmail.com>
2025-06-23 13:40:57 -0500

    Made a guess my favorate number game.

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNum = 9;
+
+            Console.WriteLine("Can You Guess My Favorate Number?");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            while (userGuess != favNum)
+            {
+                if (userGuess < favNum)
+                {
+                    Console.WriteLine("To Low!");
+                }
+                else if (userGuess > favNum)
+                {
+                    Console.WriteLine("Too High!");
+                }
+                else
+                {
+                    Console.WriteLine("Got It!");
+                }
+            }
         }
     }
 }
